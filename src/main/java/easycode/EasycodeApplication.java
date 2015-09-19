46d2b1ffdd0fa4e5b6ea74223540a4a5c041545f
cghiorzi <Chris.ghiorzi@gmail.com>
2015-09-19 15:53:43 -0400

    Added in Eclipse project info

---
+++ src/main/java/easycode/EasycodeApplication.java
@@ -0,0 +1,12 @@
+package easycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasycodeApplication.class, args);
+    }
+}
