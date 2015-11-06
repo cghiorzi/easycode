da2f99779f5962fd0cbe74bf98ed5849d564116d
Chris Ghiorzi <seethedata@gmail.com>
2015-11-06 00:10:41 -0500

    Corrected web asset serving on Cloud Foundry

--- go/easycode.go
+++ go/easycode.go
@@ -20,7 +20,7 @@ func check(function string, e error) {
 
 func responseHandler(w http.ResponseWriter, r *http.Request) {
 	location:=Location{Name: getName() }
-	t,err:=template.ParseFiles("index.html")
+	t,err:=template.ParseFiles("templates/index.tmpl")
 	check("Parse template",err)
 	t.Execute(w,location)
 }
@@ -31,5 +31,9 @@ func getName() string{
 
 func main() {
 	http.HandleFunc("/",responseHandler)
-	log.Fatal(http.ListenAndServe(":80",nil))
+	http.Handle("/images/",http.StripPrefix("/images/",http.FileServer(http.Dir("/home/vcap/app/images"))))
+	http.Handle("/css/",http.StripPrefix("/css/",http.FileServer(http.Dir("/home/vcap/app/css"))))
+	http.Handle("/fonts/",http.StripPrefix("/fonts/",http.FileServer(http.Dir("/home/vcap/app/fonts"))))
+	http.Handle("/js/",http.StripPrefix("/js/",http.FileServer(http.Dir("/home/vcap/app/js"))))
+	log.Fatal(http.ListenAndServe(":8080",nil))
 }
